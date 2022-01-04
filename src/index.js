72bcde04a9c6482516cddc9185f800e628f89757
Dmytro Yakymenko <yakimenkodmitriy0@gmail.com>
2022-01-04 09:25:32 +0200

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = n.toString();
+    if (n[0] == "-") {
+        n = n.slice(1);
+    }
+    var reverseArr = "";
+    for (var i = n.length - 1; i >= 0; i--) {
+        reverseArr += n[i];
+    }
+    return +reverseArr;
+};
